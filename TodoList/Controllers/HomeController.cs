9f757b0944f8b5c6f651db79d4cfee6a1e05c9b4
Václav Machovský <vmach.projects@gmail.com>
2023-04-05 16:46:50 +0200

    Json delete method, js onclick

--- TodoList/Controllers/HomeController.cs
+++ TodoList/Controllers/HomeController.cs
@@ -5,70 +5,85 @@ using TodoList.Models.ViewModels;
 
 namespace TodoList.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            var todoListViewModel = GetAllTodos();
-            return View(todoListViewModel);
-        }
+		public IActionResult Index()
+		{
+			var todoListViewModel = GetAllTodos();
+			return View(todoListViewModel);
+		}
 
-        internal TodoViewModel GetAllTodos()
-        {
-            List<TodoItem> todoList = new List<TodoItem>();
+		internal TodoViewModel GetAllTodos()
+		{
+			List<TodoItem> todoList = new List<TodoItem>();
 
-            using (var connection = new SqliteConnection("data source=tododb.sqlite"))
-            {
-                using (var tableCmd = connection.CreateCommand())
-                {
-                    connection.Open();
-                    tableCmd.CommandText = "Select * FROM todo";
+			using (var connection = new SqliteConnection("data source=tododb.sqlite"))
+			{
+				using (var tableCmd = connection.CreateCommand())
+				{
+					connection.Open();
+					tableCmd.CommandText = "Select * FROM todo";
 
-                    using (var reader = tableCmd.ExecuteReader())
-                    {
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                todoList.Add(new TodoItem { Id = reader.GetInt32(0), Name = reader.GetString(1) });
-                            }
-                        }
-                        else // returning empty viewmodel
-                        {
-                            return new TodoViewModel { TodoList = todoList };
-                        }
-                    }
-                }
-            }
-            return new TodoViewModel { TodoList = todoList };
-        }
+					using (var reader = tableCmd.ExecuteReader())
+					{
+						if (reader.HasRows)
+						{
+							while (reader.Read())
+							{
+								todoList.Add(new TodoItem { Id = reader.GetInt32(0), Name = reader.GetString(1) });
+							}
+						}
+						else // returning empty viewmodel
+						{
+							return new TodoViewModel { TodoList = todoList };
+						}
+					}
+				}
+			}
+			return new TodoViewModel { TodoList = todoList };
+		}
 
-        public RedirectResult Insert(TodoItem todo)
-        {
-            using (SqliteConnection connection = new SqliteConnection("data source=tododb.sqlite"))
-            {
-                using (SqliteCommand tableCmd = connection.CreateCommand())
-                {
-                    connection.Open();
-                    tableCmd.CommandText = $"INSERT INTO todo (Name) VALUES ('{todo.Name}')";
-                    try
-                    {
-                        tableCmd.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-            }
-            return Redirect("https://localhost:7253/");
-        }
-    }
+		public RedirectResult Insert(TodoItem todo)
+		{
+			using (SqliteConnection connection = new SqliteConnection("data source=tododb.sqlite"))
+			{
+				using (SqliteCommand tableCmd = connection.CreateCommand())
+				{
+					connection.Open();
+					tableCmd.CommandText = $"INSERT INTO todo (Name) VALUES ('{todo.Name}')";
+					try
+					{
+						tableCmd.ExecuteNonQuery();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+			}
+			return Redirect("https://localhost:7253/");
+		}
+
+		public JsonResult Delete(int id)
+		{
+			using (var connection = new SqliteConnection("datasource=tododb.sqlite"))
+			{
+				using (var tableCmd = connection.CreateCommand())
+				{
+					connection.Open();
+					tableCmd.CommandText = $"DELETE FROM todo WHERE Id = '{id}'";
+					tableCmd.ExecuteNonQuery();
+				}
+
+			}
+			return Json(new { });
+		}
+	}
 }
--- TodoList/wwwroot/js/site.js
+++ TodoList/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function deleteTodo(i)
+{
+    $.ajax({
+        url: 'Home/Delete',
+        type: 'POST',
+        data: {
+            id: i
+        },
+        success: function () {
+            window.location.reload();
+        }
+        })
+}
