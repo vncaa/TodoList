a4e942659dad84368e718aa23876623aa4ad3610
Václav Machovský <vmach.projects@gmail.com>
2023-04-05 16:21:04 +0200

    Added view model with a list

--- TodoList/Controllers/HomeController.cs
+++ TodoList/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using TodoList.Models;
+using TodoList.Models.ViewModels;
 
 namespace TodoList.Controllers
 {
@@ -15,14 +16,45 @@ namespace TodoList.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var todoListViewModel = GetAllTodos();
+            return View(todoListViewModel);
+        }
+
+        internal TodoViewModel GetAllTodos()
+        {
+            List<TodoItem> todoList = new List<TodoItem>();
+
+            using (var connection = new SqliteConnection("data source=tododb.sqlite"))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = "Select * FROM todo";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                todoList.Add(new TodoItem { Id = reader.GetInt32(0), Name = reader.GetString(1) });
+                            }
+                        }
+                        else // returning empty viewmodel
+                        {
+                            return new TodoViewModel { TodoList = todoList };
+                        }
+                    }
+                }
+            }
+            return new TodoViewModel { TodoList = todoList };
         }
 
         public RedirectResult Insert(TodoItem todo)
         {
-            using(SqliteConnection connection = new SqliteConnection("data source=tododb.sqlite"))
+            using (SqliteConnection connection = new SqliteConnection("data source=tododb.sqlite"))
             {
-                using(SqliteCommand tableCmd = connection.CreateCommand())
+                using (SqliteCommand tableCmd = connection.CreateCommand())
                 {
                     connection.Open();
                     tableCmd.CommandText = $"INSERT INTO todo (Name) VALUES ('{todo.Name}')";
---
+++ TodoList/Models/ViewModels/TodoViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TodoList.Models.ViewModels
+{
+    public class TodoViewModel
+    {
+    }
+}
