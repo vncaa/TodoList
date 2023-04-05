1d8655880acfbac6df32cd76e994bddba0c2c2fc
Václav Machovský <vmach.projects@gmail.com>
2023-04-05 15:59:33 +0200

    Add project files.

---
+++ TodoList/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Insert(TodoItem todo)
+        {
+            using(SqliteConnection connection = new SqliteConnection("data source=tododb.sqlite"))
+            {
+                using(SqliteCommand tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (Name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TodoList/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoList.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
